ba0af1a205cbd38b52e64a3c2fa5e09746ad2811
filipeas <felipealvessampaio@hotmail.com>
2024-06-06 16:41:12 +0000

    test commit via docker

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -5,6 +5,15 @@ echo "This script will run every time the container is (re)started."
 
 
 # Install dependencies
-
+# SAM
+pip install git+https://github.com/facebookresearch/segment-anything.git
+# Transformers
+pip install -q git+https://github.com/huggingface/transformers.git
+# Datasets to prepare data and monai if you want to use special loss functions
+pip install datasets
+pip install -q monai
+# Patchify to divide large images into smaller patches for training. (Not necessary for smaller images)
+pip install patchify
+pip install opencv-python
 
 echo "Done."
